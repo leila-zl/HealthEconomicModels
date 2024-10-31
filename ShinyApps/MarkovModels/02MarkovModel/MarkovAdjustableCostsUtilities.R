7caed73b164a3b96aa8769aee525f48619778024
Leila Zakka <leilazakka@gmail.com>
2024-10-31 11:53:03 +0000

    update

--- ShinyApps/MarkovModels/02MarkovModel/MarkovAdjustableCostsUtilities.R
+++ ShinyApps/MarkovModels/02MarkovModel/MarkovAdjustableCostsUtilities.R
@@ -15,7 +15,7 @@ ui <- fluidPage(
       numericInput("cohort_size", "Enter Cohort size:", value = 1000, min = 10, max = 100000),
       
       # Slider inputs for number of PSA iterations and cycle length
-      sliderInput("psa_iterations", "Number of PSA iterations:", min = 1, max = 1000, value = 10),
+      sliderInput("psa_iterations", "Number of PSA iterations:", min = 1, max = 1000, value = 500),
       sliderInput("cycle_length", "Choose Cycle Length:", min = 1, max = 100, value = 40),
       
       # Adjustable sliders for health state costs and utility values
@@ -41,28 +41,28 @@ server <- function(input, output) {
     
     # Base case parameters
     params_basecase <- data.frame(
-      p_Healthy_Diseased = 0.03,  # Probability of transitioning from healthy to diseased
-      p_Healthy_Dead     = 0.01,  # Probability of transitioning from healthy to dead
-      p_Diseased_Dead    = 0.05,  # Probability of transitioning from diseased to dead
+      p_healthy_diseased = 0.03,  # Probability of transitioning from healthy to diseased
+      p_healthy_dead     = 0.01,  # Probability of transitioning from healthy to dead
+      p_diseased_dead    = 0.05,  # Probability of transitioning from diseased to dead
       
-      cost_Healthy  = input$cost_h,   # Cost for healthy state
-      Cost_Diseased = input$cost_d,   # Cost for diseased state
-      u_Healthy  = input$utility_h, # Utility value for healthy state
-      u_Diseased = input$utility_d  # Utility value for diseased state
+      cost_healthy  = input$cost_h,   # Cost for healthy state
+      cost_diseased = input$cost_d,   # Cost for diseased state
+      u_healthy  = input$utility_h, # Utility value for healthy state
+      u_diseased = input$utility_d  # Utility value for diseased state
     )
     
     # PSA parameters (probabilistic sampling for sensitivity analysis)
     params_psa_samples <- data.frame(
       # Transition probabilities with sampling from beta distribution
-      p_Healthy_Diseased = rbeta(input$psa_iterations, 3, 97),
-      p_Healthy_Dead     = rbeta(input$psa_iterations, 1, 99),
-      p_Diseased_Dead    = rbeta(input$psa_iterations, 5, 95),
+      p_healthy_diseased = rbeta(input$psa_iterations, 3, 97),
+      p_healthy_dead     = rbeta(input$psa_iterations, 1, 99),
+      p_diseased_dead    = rbeta(input$psa_iterations, 5, 95),
       
       # Variable costs and utilities generated using gamma and beta distributions
-      cost_Healthy  = rgamma(input$psa_iterations, shape = 25, scale = input$cost_h / 25),
-      Cost_Diseased = rgamma(input$psa_iterations, shape = 25, scale = input$cost_d / 25),
-      u_Healthy  = rbeta(input$psa_iterations, (input$utility_h) * 100, 10),
-      u_Diseased = rbeta(input$psa_iterations, (input$utility_d) * 100, 40)
+      cost_healthy  = rgamma(input$psa_iterations, shape = 25, scale = input$cost_h / 25),
+      cost_diseased = rgamma(input$psa_iterations, shape = 25, scale = input$cost_d / 25),
+      u_healthy  = rbeta(input$psa_iterations, (input$utility_h) * 100, 10),
+      u_diseased = rbeta(input$psa_iterations, (input$utility_d) * 100, 40)
     )
     
     # Markov model function to easily calculate base case and PSA payoffs from parameters
@@ -71,19 +71,19 @@ server <- function(input, output) {
         # Transition probability matrix (3x3), defining transition probabilities for each health state
         m_probability <- matrix(0, 
                                 nrow = n_states, ncol = n_states, 
-                                dimnames = list(from = v_state_names, to = v_state_names))
+                                dimnames = list(from = state_names, to = state_names))
         # Populate matrix with transition probabilities
-        m_probability["Healthy",  "Healthy"]  <- 1 - p_Healthy_Diseased - p_Healthy_Dead
-        m_probability["Healthy",  "Diseased"] <- p_Healthy_Diseased
-        m_probability["Healthy",  "Dead"]     <- p_Healthy_Dead
-        m_probability["Diseased", "Diseased"] <- 1 - p_Diseased_Dead
-        m_probability["Diseased", "Dead"]     <- p_Diseased_Dead
+        m_probability["Healthy",  "Healthy"]  <- 1 - p_healthy_diseased - p_healthy_dead
+        m_probability["Healthy",  "Diseased"] <- p_healthy_diseased
+        m_probability["Healthy",  "Dead"]     <- p_healthy_dead
+        m_probability["Diseased", "Diseased"] <- 1 - p_diseased_dead
+        m_probability["Diseased", "Dead"]     <- p_diseased_dead
         m_probability["Dead",     "Dead"]     <- 1
         
         # Define the state membership matrix for each cycle
         state_membership <- array(NA_real_, 
                                   dim = c(input$cycle_length, n_states), 
-                                  dimnames = list(cycle = 1:input$cycle_length, state = v_state_names))
+                                  dimnames = list(cycle = 1:input$cycle_length, state = state_names))
         # Initialize with full cohort in the healthy state at the start
         state_membership[1,] <- c(input$cohort_size, 0, 0)
         for (i in 2:input$cycle_length) {
@@ -93,12 +93,12 @@ server <- function(input, output) {
         # Define payoff matrix for costs and QALYs in each state
         m_payoffs <- array(0,
                            dim = c(n_states, 2),
-                           dimnames = list(states = v_state_names, payoffs = c("cost", "qaly")))
+                           dimnames = list(states = state_names, payoffs = c("cost", "qaly")))
         # Populate with base case costs and QALYs
-        m_payoffs["Healthy","cost"] <- cost_Healthy
-        m_payoffs["Healthy","qaly"] <- u_Healthy
-        m_payoffs["Diseased","cost"] <- Cost_Diseased
-        m_payoffs["Diseased","qaly"] <- u_Diseased
+        m_payoffs["Healthy","cost"] <- cost_healthy
+        m_payoffs["Healthy","qaly"] <- u_healthy
+        m_payoffs["Diseased","cost"] <- cost_diseased
+        m_payoffs["Diseased","qaly"] <- u_diseased
         
         # Calculate costs and QALYs per cycle by multiplying state membership and payoff matrix
         payoff_trace <- state_membership %*% m_payoffs
